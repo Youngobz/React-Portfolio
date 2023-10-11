4eed4a98047c8ef0a159ba683792d539be166901
Youngobz <133522178+Youngobz@users.noreply.github.com>
2023-10-11 16:31:08 +1100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate, Switch } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
+import About from "./pages/About";
+
+const routeList = [{ path: "/about", component: <About /> }];
 
 function App() {
   return (
     <div className="App">
       <Header className="header" />
       <Routes>
-        <Route exact path="/" element={Home} />
+        <Route exact path="/" element={<Navigate replace to="/about" />} />
+        {routeList.map((route) => (
+          <Route key={route.path} path={route.path} element={route.component} />
+        ))}
       </Routes>
       <Footer />
     </div>
