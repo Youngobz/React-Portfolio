0edf8e5442c87b140f88aa854bd3c9466eb6df24
Youngobz <133522178+Youngobz@users.noreply.github.com>
2023-10-11 12:26:28 +1100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Home from "./pages/Home";
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header className="header" />
+      <Routes>
+        <Route exact path="/" element={Home} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
